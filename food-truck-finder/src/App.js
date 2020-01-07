6286079c63e591b01d5c24dc0de06fd3be11f831
Nazim Uddin <umnazim@gmail.com>
2020-01-06 19:58:05 -0800

    Home page excluded

--- food-truck-finder/src/App.js
+++ food-truck-finder/src/App.js
@@ -1,41 +1,37 @@
-import React, { useEffect } from 'react';
-import Navbar from'./components/navigation/Navbar'
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import React, { useEffect } from "react";
+import Navbar from "./components/navigation/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import Navlinks from './components/navigation/Navlinks';
-import Bookevents from './components/bookevents/vendor.container'
-import Recipes from './components/recipes/recipes.container';
-import Home from './components/home/home.container';
-import Maps from './components/MapContainers/MapContainers'
+import Bookevents from "./components/bookevents/vendor.container";
+import Recipes from "./components/recipes/recipes.container";
 
-import './';
+// Home page disabled
+// import Home from './components/home/home.container';
+import Maps from "./components/MapContainers/MapContainers";
 
-import './App.scss';
-import { connect } from 'react-redux';
-import * as actions from './actions'
+import "./";
 
+import "./App.scss";
+import { connect } from "react-redux";
+import * as actions from "./actions";
 
-
-function App(){
-  return(
+function App() {
+  return (
     <Router>
       <div>
-      <Navbar/>
-      <Switch>
-      
-        {/*<Route exact path="/home" components={Home}/>*/}
-        <Route exact path="/recipes" component={Recipes}/>
-        <Route exact path="/bookevents" component={Bookevents}/>
-        <Route exact path="/home" component={Home}/>
-        <Route exact path= "/map" component ={Maps}/>
-        
+        <Navbar />
+        <Switch>
+          {/*<Route exact path="/home" components={Home}/>*/}
+          <Route exact path="/recipes" component={Recipes} />
+          <Route exact path="/bookevents" component={Bookevents} />
 
-      </Switch>
-    {/* <Navlinks/> */}
-
-    </div>
+          {/* <Route exact path="/home" component={Home}/> */}
+          <Route exact path="/map" component={Maps} />
+        </Switch>
+        {/* <Navlinks/> */}
+      </div>
     </Router>
   );
 }
- 
 
-export default connect( null, actions )(App);
+export default connect(null, actions)(App);
